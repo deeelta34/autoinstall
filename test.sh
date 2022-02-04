9161e6621199e140aec6dc020b2dc6e95f39dc64
deeelta34 <83288114+deeelta34@users.noreply.github.com>
2022-02-04 18:21:15 +0300

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt update
+apt upgrade
+apt install nano
+apt-get install apache2
+apt-get install ufw
+ufw enable
+ufw app list
+ufw allow 'Apache Full'
+systemctl stop apache2
+systemctl start apache2
+apt-get install xrdp
+systemctl enable xrdp
+apt-get install xfce4 xfce4-terminal
+sed -i.bak '/fi/a #xrdp multiple users configuration n xfce-session n' /etc/xrdp/startwm.sh
+apt-get install mate-core mate-desktop-environment mate-notification-daemon
+sed -i.bak '/fi/a #xrdp multiple users configuration n mate-session n' /etc/xrdp/startwm.sh
+ufw allow from any to any port 3389 proto tcp
+./etc/init.d/xrdp restart
+apt-get -y install xfce4 xfce4-terminal tango-icon-theme firefox
