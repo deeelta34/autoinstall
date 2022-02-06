66264a19c9d33dcad264cfe88642135ee837e782
deeelta34 <83288114+deeelta34@users.noreply.github.com>
2022-02-06 14:50:58 +0300

    Add files via upload

--- test.sh
+++ test.sh
@@ -5,7 +5,6 @@ apt install tasksel -y
 tasksel
 systemctl set-default graphical.target
 apt install xrdp -y
-systemctl status xrdp
 usermod -a -G ssl-cert xrdp
 systemctl restart xrdp
 ufw allow 3389
@@ -13,3 +12,4 @@ adduser admin
 gpasswd -a admin sudo
 adduser user
 wget "https://github.com/dolphinrucom/anty-releases/releases/download/v2022.7.10/dolphin-anty-linux-x86_64-latest.AppImage"
+chmod u+x dolphin-anty-linux-x86_64-latest.AppImage
